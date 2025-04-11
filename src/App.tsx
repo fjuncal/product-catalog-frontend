ab06e6c6374a355e8249fbb270e444ed7be58f77
Fellipe Juncal <fjuncal7@gmail.com>
2025-04-10 23:29:59 -0300

    Creating routes in my app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Route, BrowserRouter, Routes } from "react-router";
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/products" element={<ProductsPage />} />
+					<Route path="/categories" element={<CategoriesPage />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
-export default App
+export default App;
